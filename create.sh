189943eda9d97d5b613a093f38752707a0c516f4
Jeff Parla <90476759+parlajatwit@users.noreply.github.com>
2023-02-22 21:09:08 -0500

    generate links to other articles by newest post

--- create.sh
+++ create.sh
@@ -4,6 +4,7 @@ MD_CONVERTER="smu"
 ARTICLES_PATH="articles"
 BUILD_PATH="build"
 TEMPLATE="template.html"
+LINKS="links"
 
 GeneratePage () {
 	export TITLE=$(sed -n '1p' $1)
@@ -11,10 +12,20 @@ GeneratePage () {
 	export DATE=$(sed -n '3p' $1)
 	FNAME=$(echo $1 | xargs basename -s .md)
 	FPATH="$BUILD_PATH/$FNAME.html"
-	echo "$FPATH"
 	export CONTENT=$(sed -n '4,$p' $1 | $MD_CONVERTER)
+	export FOOTER=$(cat $LINKS | sort | sed -e 's/$/  /' | $MD_CONVERTER) 
 	envsubst < $TEMPLATE > $FPATH
-	echo "done"
 }
 
-find $ARTICLES_PATH/* | while read file; do GeneratePage "$file"; done
+GenerateLinks() {
+	TITLE=$(sed -n '1p' $1)
+	AUTHOR=$(sed -n '2p' $1)
+	DATE=$(sed -n '3p' $1)
+	FNAME=$(echo $1 | xargs basename -s .md)
+	echo "$DATE - $AUTHOR - [$TITLE]($FNAME.html)" >> "$LINKS"
+}
+
+rm $LINKS
+find "$ARTICLES_PATH"/* | while read file; do GenerateLinks "$file"; done
+find "$ARTICLES_PATH"/* | while read file; do GeneratePage "$file"; done
+echo "done"
